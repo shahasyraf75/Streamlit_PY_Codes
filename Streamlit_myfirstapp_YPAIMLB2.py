f4dfebc8aaccdb2e084eef854002a75a5b3ad85f
MSALIVE <shahasyraf75@gmail.com>
2025-11-06 04:23:19 +0800

    Add files via upload

---
+++ Streamlit_myfirstapp_YPAIMLB2.py
@@ -0,0 +1,11 @@
+# ==============================================================
+# 📘 Streamlit_myfirstapps --> Python Basic To Advanced
+# Author: Muhammad Shah Asyraf
+# Credit: Yash Sharma
+# Artificial Intelligence & Machine Learning Course
+# ==============================================================
+
+import streamlit as st
+st.title('Hello World')
+st.write('Hello! This is my first Streamlit web page.')
+
